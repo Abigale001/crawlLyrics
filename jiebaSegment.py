44718a23781f8fb1cad063ab777f332a2ff950db
Abigale <liyicong123@outlook.com>
2018-09-07 11:30:58 +0800

    Add files via upload

---
+++ jiebaSegment.py
@@ -0,0 +1,17 @@
+import jieba_fast as jieba
+
+#delete the stop words
+stop = [line.strip().decode('utf-8') for line in open('//home//liyicong//Desktop//lyrics//stop_words_zh.txt','r').readlines()]
+
+with open("//home//liyicong//Desktop//lyrics//lyrics.txt","r") as lyricFile:
+    segmentLyrics = open('//home//liyicong//Desktop//lyrics//SegmentLyrics.txt', 'a+')
+    for line in lyricFile:
+        seg_list = jieba.cut(line, cut_all=False)  ##type:generator
+        for seg in seg_list:
+            if seg not in stop:
+                segmentLyrics.write(seg.encode('utf-8'))
+                if seg != '\n':
+                    segmentLyrics.write(' ')
+    segmentLyrics.close()
+
+
