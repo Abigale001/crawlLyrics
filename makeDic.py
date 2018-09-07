0d19fec7e6874b80f1532a1594b98493ad7b06db
Abigale <liyicong123@outlook.com>
2018-09-07 11:31:23 +0800

    Add files via upload

---
+++ makeDic.py
@@ -0,0 +1,31 @@
+
+wordList = []
+worddic = {}
+with open("//home//liyicong//Desktop//lyrics//SegmentLyrics.txt","r") as SegmentLyrics:
+    for line in SegmentLyrics:
+        listLyric = line.split(' ')
+        print listLyric
+        for word in listLyric:
+            wordList.append(word)
+
+wordSet = set(wordList)
+wordSet.remove('\n')
+print len(wordSet)
+
+dicFile = open("//home//liyicong//Desktop//lyrics//lyrics.dict","a+")
+
+eleId = 0
+for ele in wordSet:
+    worddic[ele] = eleId
+
+    dicFile.write(ele)
+    dicFile.write(' ')
+    dicFile.write(str(eleId))
+    dicFile.write('\n')
+    eleId += 1
+
+print worddic
+print len(worddic)
+
+
+
